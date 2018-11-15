81823a09a382a36c9092e806aa7b0991d1ad14f5
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2018-11-14 23:11:08 -0800

    get photo and list photos

--- server.js
+++ server.js
@@ -11,18 +11,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Configure AWS SDK
-const aws = require("aws-sdk")
-    , S3_BUCKET = process.env.S3_BUCKET;
-
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
 aws.config.region = process.env.AWS_REGION;
+S3_BUCKET = process.env.S3_BUCKET;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 app.get('/sign-s3', (req, res) => {
-  const s3 = new aws.S3();
-  const fileName = 'wedding/' + req.query['file-name'];
+  const fileName = req.query['file-name'];
   const fileType = req.query['file-type'];
   const s3Params = {
     Bucket: S3_BUCKET,
@@ -46,6 +45,36 @@ app.get('/sign-s3', (req, res) => {
     res.write(JSON.stringify(returnData));
     res.end();
   });
+});
+
+app.get('/list-photos', (req, res) => {
+  const params = {
+    Bucket: process.env.S3_BUCKET,
+    MaxKeys: 2
+  };
+
+  s3.listObjects(params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+
+    console.log(data);
+  });
+  res.send("hello world");
+});
+
+app.get('/get-photos', (req, res) => {
+
+  s3.getObject({Bucket: process.env.S3_BUCKET, Key: 'wedding/front-page-1.jpg'}, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+
+    console.log(data);
+  });
+  res.send("hello world");
 });
 
 app.listen(PORT, ()=> {
