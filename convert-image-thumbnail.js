0ea2f9cce9e0607c7a9fdad452f8f3ac25b89f49
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-03-17 22:16:46 -0700

    Created promise to execute all functions

--- convert-image-thumbnail.js
+++ convert-image-thumbnail.js
@@ -1,21 +1,30 @@
 const thumb = require('node-thumbnail').thumb;
 
-createThumbnail();
-// reduceImage();
+createThumbnail()
+    .then((result) => {
+        reduceImage();
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 function createThumbnail() {
-    thumb({
-        source: 'real-photo/',
-        destination: 'thumbnail-photo/',
-        concurrency: 4,
-        width: 400,
-        suffix: '-thumbnail',
-        overwrite: true
-    }).then(function() {
-        console.log('All done!');
-    }).catch(function(error) {
-        console.log('Error', error.toString());
-    }); 
+    return new Promise((resolve, reject) => {
+        thumb({
+            source: 'real-photo/',
+            destination: 'thumbnail-photo/',
+            concurrency: 4,
+            width: 400,
+            suffix: '-thumbnail',
+            overwrite: true
+        }).then(function() {
+            console.log('All done!');
+            resolve();
+        }).catch(function(error) {
+            // console.log('Error', error.toString());
+            reject(error.toString());
+        }); 
+    });
 }
 
 function reduceImage() {
