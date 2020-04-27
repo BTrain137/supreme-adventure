c9a703321558ccccf1f808c67d6d84cbfea9ba07
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2020-04-27 08:19:39 -0700

    Remove other project that doesn't isn't on the server anymore
    renamed project name to match folder name.

--- Nginx/ecosystem.config.js
+++ Nginx/ecosystem.config.js
@@ -14,21 +14,13 @@ module.exports = {
       },
     },
     {
-      name: "bryan-kim.com",
+      name: "bryankim.com",
       cwd: "/var/www/bryankim.com/source",
       script: "server.js",
       env_production: {
         NODE_ENV: "production",
       },
-    },
-    {
-      name: "shopify-app",
-      cwd: "/var/www/shopify-app/",
-      script: "server.js",
-      env_production: {
-        NODE_ENV: "production",
-      },
-    },
+    }
   ],
   deploy: {
     "bryankim.com" : {
@@ -37,6 +29,8 @@ module.exports = {
       host: ["ec2-35-169-209-116.compute-1.amazonaws.com"],
       ref: "origin/master",
       repo: "https://github.com/bryan89tran/supreme-adventure.git",
+      // Pm2 goes into source folder
+      // /var/www/bryankim.com/source
       path: "/var/www/bryankim.com",
       "post-deploy":
         "npm install && pm2 startOrRestart ./../../ecosystem.config.js --env production",
