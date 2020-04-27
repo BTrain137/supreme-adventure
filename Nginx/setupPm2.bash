c5f6b617849ae6c7eee5f04dc0b7b71144edfdc6
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2020-04-27 08:22:45 -0700

    Notes on after initing bash. For now must manually copy env file from local to production

--- Nginx/setupPm2.bash
+++ Nginx/setupPm2.bash
@@ -1,5 +1,5 @@
 # pm2 needs work in a folder called source cannot figure any other way
-PROJECTNAME="test"
+PROJECTNAME="bryankim.com"
 GITREPO="https://github.com/BTrain137/supreme-adventure.git"
 
 sudo mkdir -p /var/www/$PROJECTNAME/source
@@ -7,3 +7,5 @@ cd /var/www/$PROJECTNAME/source
 sudo git init
 sudo git remote add origin $GITREPO
 sudo chown -R $USER /var/www/$PROJECTNAME
+
+# After initial setup must ssh into repo and provide .env file
